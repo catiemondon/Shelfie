3452f95135e8650f71027930d370dd6809b070f8
catiemondon <Cathmond25@gmail.com>
2019-05-17 10:35:47 -0600

    got database connected to server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
+require('dotenv').config();
 const express= require('express')
+const dotenv= require('dotenv')
 const app=express()
+const massive= require('massive')
 const ctrl=require('./controller')
+
+const{SERVER_PORT, CONNECTION_STRING}= process.env
+
+
+
 app.use(express.json())
 
 
+massive(CONNECTION_STRING)
+    .then(dbInstance =>{
+        app.set('db', dbInstance)
+        console.log('Database, you go girl')
+    })
+    .catch(err =>{
+        if(err) throw err;
+    })
 
 
-const port=3333;
-app.listen(port, ()=>{
+app.listen(SERVER_PORT, ()=>{
     console.log(`Slayyy server, S L A Y`)
 })
