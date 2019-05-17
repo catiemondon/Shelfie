c4484192f13ac0638926af27fea03a4fd0b635e6
catiemondon <Cathmond25@gmail.com>
2019-05-17 10:11:39 -0600

    basic react component structure created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 import './App.css';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Form from './Components/Form/Form';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+     <Dashboard />
+     <Form />
+     <Header />
      
     </div>
   );
---
+++ src/Components/Form/Form.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class Form extends Component{
+    constructor(){
+        super()
+    }
+
+
+
+
+
+
+    render(){
+        return(
+            <h1> Form </h1>
+        )
+    }
+}
+
+export default Form
